5cfb64a83a0ceeea43e6d59ba64757d646caf566
FlDeLima <florian.de_lima@hotmail.com>
2018-10-05 23:10:27 -0400

    ajout des classes

---
+++ GameJam Octobre 2018/Assets/Scripts/Campement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Campement : MonoBehaviour {
+
+    public int nbSurvivant;
+    public bool survivantContent;
+    public List<GameObject> batiments; // liste des batiment dans la scene a lier dans l'editeur
+    public List<Soldat> soldats;
+    public Inventaire inventaire;
+    public List<Mission> missionsDisponible;
+
+    public GameObject SurvivantPrefab;
+    private List<GameObject> survivants;
+
+	// Use this for initialization
+	void Start () {
+        nbSurvivant = 2;
+
+
+        for (int i=0;i<nbSurvivant;i++)
+        {
+            Instantiate(SurvivantPrefab); //to do : pop random ?
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+	}
+
+    void RecolterBois(int nbSurvivantEnvoye)
+    {
+
+    }
+
+    void NewDay()
+    {
+        if(Inventaire.Instance.qteNourriture>=nbSurvivant)
+        {
+            for (int i = 0; i < nbSurvivant; i++)
+            {
+                Inventaire.Instance.qteNourriture -= nbSurvivant;
+            }
+        }
+        else
+        {
+            int nourritureManquante = nbSurvivant - Inventaire.Instance.qteNourriture;
+            Inventaire.Instance.qteNourriture = 0;
+            Famine(nourritureManquante);
+        }
+        
+    }
+
+    //perte de survivant
+    void Famine(int _nourritureManquante)
+    {
+        
+
+
+    }
+}
---
+++ GameJam Octobre 2018/Assets/Scripts/Inventaire.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventaire : MonoBehaviour {
+
+    //singleton
+    private static Inventaire instance;
+    public static Inventaire Instance { get { return instance; } }
+
+    public int qtePierre;
+    public int qteBois;
+    public int qteMetal;
+    public int qteNourriture;
+    // Use this for initialization
+    void Start () {
+
+
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
