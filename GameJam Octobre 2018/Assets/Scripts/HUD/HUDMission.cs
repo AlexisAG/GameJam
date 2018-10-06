3df0853343630fa275314e0ff312f253d946276e
AlexisAG <thedeath475@gmail.com>
2018-10-06 15:23:57 -0400

    Menu principal ok

--- GameJam Octobre 2018/Assets/Scripts/HUD/HUDMission.cs
+++ GameJam Octobre 2018/Assets/Scripts/HUD/HUDMission.cs
@@ -10,12 +10,6 @@ public class HUDMission : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake () {
-        CampementData.Instance.soldats.Add(new Soldat(TypeCombattant.nomTypeCombattant.Assassin));
-        GenerationMissionDefense.GenerateMission();
-        GenerationMissionDefense.GenerateMission();
-        GenerationMissionDefense.GenerateMission();
-        GenerationMissionDefense.GenerateMission();
-        GenerationMissionDefense.GenerateMission();
 
         for (int i = 0; i < MissionsPins.Count; i++)
             MissionsPins[i].transform.localPosition = CampementData.Instance.missionsDisponible[i].GetPositionOnMap();
